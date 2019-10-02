ec7c4d0af149ff9b68951ba7450a46be7c2d4517
Kevin Hayden <kevin.fhayden@gmail.com>
2019-10-02 00:43:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       self.status = "complete"
     else
       self.status = "rejected"
-      "Transaction rejected"
+      "Transaction rejected. Please check your account balance."
   end
   
   def reverse_transaction
