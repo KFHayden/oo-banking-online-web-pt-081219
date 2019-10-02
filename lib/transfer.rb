4bca76ffee6e77ee832ab15f8cfe32c0f1839626
Kevin Hayden <kevin.fhayden@gmail.com>
2019-10-02 00:39:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,6 +3,8 @@ class Transfer
   attr_accessor :sender, :receiver, :amount, :status
   
   def initialize(sender, receiver, amount)
-    
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
   
 end
